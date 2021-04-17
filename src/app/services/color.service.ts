a40c639864df45545c695196322fc146cf97de3d
talhaaersoy <63413850+talhaaersoy@users.noreply.github.com>
2021-04-17 15:13:24 +0300

    List Colors

--- src/app/services/color.service.ts
+++ src/app/services/color.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Color } from '../models/color';
+import { DataResponseModel } from '../models/dataResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ColorService {
-
-  constructor() { }
+  apiUrl = "https://localhost:44371/api/colors/getall" 
+  constructor(private httpClient:HttpClient) { }
+  getColors() : Observable<DataResponseModel<Color[]>>{
+      return this.httpClient.get<DataResponseModel<Color[]>>(this.apiUrl);
+  }
 }
