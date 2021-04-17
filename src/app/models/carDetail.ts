3396d6123673d66387e49e2dc5487ca83b6248cf
talhaaersoy <63413850+talhaaersoy@users.noreply.github.com>
2021-04-17 14:43:44 +0300

    Edit CarComponent

--- src/app/models/carDetail.ts
+++ src/app/models/carDetail.ts
@@ -1,8 +1,9 @@
 export interface CarDetail{
-    id:number;
+    carId:number;
     carName:string;
     brandName:string;
     colorName:string;
     modelYear:number;
     dailyPrice:number;
+    description:string;
 }
